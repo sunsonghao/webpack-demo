3854c221025ee3dea9ad263d1aa3cc59c12336af
sunsonghao <sunsonghao@sinosoft.com.cn>
2020-05-21 16:39:54 +0800

    1-4使用loader

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// 不认识css文件，需要loader解析
+require('./main.css');
 const show = require('./show');
 
 show("Webpack");
--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,21 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist')
+  },
+  module: {
+    // 一组规则，告诉webpack遇到哪些文件用哪些loader加载和转换
+    rules: [
+      {
+        test: /\.css$/,
+        // 在引用css的地方require('style-loader!css-loader?minimize!.main.css')
+        // 或者
+        use: ['style-loader', {
+          loader: 'css-loader',
+          options: {
+            // minimize: true
+          }
+        }]
+      }
+    ]
   }
 }
