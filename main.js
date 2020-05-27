780857c6c63b45bd389d4d7ae22f00a7ae37300d
sunsonghao <songhaosun@gmail.com>
2020-05-27 16:43:21 +0800

    1-6使用devServer

--- main.js
+++ main.js
@@ -2,4 +2,8 @@
 require('./main.css');
 const show = require('./show');
 
-show("Webpack");
+let content = `
+    DevServer 会启动一个 HTTP 服务器用于服务网页请求，同时会帮助启动 Webpack ，并接收 Webpack 发出的文件更变信号，通过 WebSocket 协议自动刷新网页做到实时预览
+  `
+show(content);
+// show('123');
