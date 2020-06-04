37c6f41439c8356997ddda4645cb5ff76d1abf0f
sunsonghao <songhaosun@gmail.com>
2020-06-04 12:33:26 +0800

    2-2-1ouput配置

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,10 @@ const MiniCssTextPlugin = require('mini-css-extract-plugin');
 // commonjs 规范,导出配置项
 module.exports = {
   mode: 'none',
+
   // webpack在寻找相对文件路径时，会以context为根目录(绝对路径,默认为CWD)。也可以配置在启动命令后
   context: path.resolve(__dirname, 'src'),
+
   // entry: './main.js',
   /* entry: {
     main: './main.js' // key是chunk的名称，描述chunk的入口
@@ -13,10 +15,22 @@ module.exports = {
   // 动态入口, 设置为一个函数动态返回配置
   // entry: () =>'./main.js', // 同步函数
   entry: () => new Promise((resolve) => resolve(['./main.js'])), // 异步函数
+
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
+    // filename: 'bundle.js',
+    // 借助模板和变量，[id | name | hash(唯一标识id的hash) | chunkhash]
+    filename: '[name].js',
+    // 非入口(运行过程中产生，使用 CommonChunkPlugin、使用 import('path/to/module') 动态加载等时)chunk在输出时的文件名称
+    chunkFilename: '[id].js',
+    // 绝对路径
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '',
+    // 异步加载, 通过JSONP实现，JSONP原理动态向html插入script实现。
+    // 设置异步加载标签的crossorigin属性
+    crossOriginLoading: false, 
+
   },
+
   module: {
     // 一组规则，告诉webpack遇到哪些文件用哪些loader加载和转换
     rules: [
