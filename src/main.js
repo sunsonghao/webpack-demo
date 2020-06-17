473c170592ed7b9cfe614dc5d547fae61ce59c83
sunsonghao <sunsonghao@sunsonghaodeMac-mini.local>
2020-06-17 17:08:46 +0800

    babel bug-fix

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 // 不认识css文件，需要loader解析
 require('./main.css');
 const show = require('./show');
+class Point {
+  constructor (x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 let content = `
   DevServer 会启动一个 HTTP 服务器用于服务网页请求，同时会帮助启动 Webpack ，并接收 Webpack 发出的文件更变信号，通过 WebSocket 协议自动刷新网页做到实时预览
