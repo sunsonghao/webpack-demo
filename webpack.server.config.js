9285b45e72e6e81871e7ef3f68032f67da3bd29c
sunsonghao <sunsonghao@sunsonghaodeMac-mini.local>
2020-06-18 18:10:10 +0800

    3-11同构应用(only link)

---
+++ webpack.server.config.js
@@ -0,0 +1,4 @@
+// https://webpack.wuhaolin.cn/3%E5%AE%9E%E6%88%98/3-11%E6%9E%84%E5%BB%BA%E5%90%8C%E6%9E%84%E5%BA%94%E7%94%A8.html
+// 服务端渲染和预渲染vue-ssr & prerender(prerender-spa-plugin, https://github.com/chrisvfritz/prerender-spa-plugin)
+    // https://ssr.vuejs.org/zh/#%E6%9C%8D%E5%8A%A1%E5%99%A8%E7%AB%AF%E6%B8%B2%E6%9F%93-vs-%E9%A2%84%E6%B8%B2%E6%9F%93-ssr-vs-prerendering
+    // https://ssr.vuejs.org/zh/guide/build-config.html
